4dc49d98905bf5a6162d5d025c5d2e4b53e85174
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-05-04 20:13:35 +0200

    Update vat_ledger

--- l10n_ar_account_vat_ledger/models/account_invoice_line.py
+++ l10n_ar_account_vat_ledger/models/account_invoice_line.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import models
-from . import wizards
+from odoo import models, fields
+
+
+# for performance
+# TODO this should be suggested to odoo by a PR
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    invoice_id = fields.Many2one(
+        auto_join=True,
+    )
--- l10n_ar_account_vat_ledger/wizards/__init__.py
+++ l10n_ar_account_vat_ledger/wizards/__init__.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import models
-from . import wizards
+from . import res_config_settings
