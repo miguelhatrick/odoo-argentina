3532e0244e9c1079afee5940506fbd814c575ee4
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-05-08 16:53:12 +0200

    Fixing vat_ledget xlsx

--- l10n_ar_account_vat_ledger/__init__.py
+++ l10n_ar_account_vat_ledger/__init__.py
@@ -3,6 +3,5 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import account
-from . import account_invoice
-from . import account_vat_report
+from . import models
+from . import wizards
